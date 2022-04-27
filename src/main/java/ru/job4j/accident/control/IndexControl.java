6da30daf8cc72339dcbbca852f1ae231f43d817d
Ruslan Fajziev <rus23f@mail.ru>
2022-04-28 01:05:22 +0300

    1. Загрузка данных в вид

--- src/main/java/ru/job4j/accident/control/IndexControl.java
+++ src/main/java/ru/job4j/accident/control/IndexControl.java
@@ -4,10 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class IndexControl {
     @GetMapping("/")
     public String index(Model model) {
+        List<String> lst = new ArrayList<>();
+        lst.add("Вася");
+        lst.add("Петя");
+        lst.add("Саня");
+        lst.add("Вовчик");
+        lst.add("Руслан");
+        model.addAttribute("user", "Petr Arsentev");
+        model.addAttribute("names", lst);
         return "index";
     }
 }
